e5a2d5a8f25afbd8df00d86e3b328583f4d5e977
Sille Van Landschoot <info@sillevl.be>
2015-11-18 11:10:19 +0100

    removed logger

--- server.js
+++ server.js
@@ -19,15 +19,6 @@ var allowCrossDomain = function(req, res, next) {
 app.use(bodyParser.json())
 app.use(allowCrossDomain);
 
-// Log all requests
-app.use(function(req, res, next){
-  console.log('[' + Date.now() + '] Request received');
-  console.log('    url: ' + req.originalUrl);
-  console.log('    params: ' + JSON.stringify(req.params));
-  console.log('    body: ' + JSON.stringify(req.body));
-  next();
-});
-
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
